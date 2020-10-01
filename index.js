39dd3c2ce90b7d2555a8551c19a52d79cf00e4dc
Haz-git <zhouharry1@gmail.com>
2020-10-01 12:36:09 -0700

    Added Module

--- index.js
+++ index.js
@@ -2,26 +2,12 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplates');
 
 //Top-level code: This is a sync function is executed once. This is to prevent users from cause re-reading of file per refresh.
 
 //__dirname is the directory where this (index.js) current file is located.
 //We are synchronously reading our html templates here.
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    }
-    return output;
-}
 
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
