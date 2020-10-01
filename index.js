0e15009faef1958e269e87d6e2251518b787618c
Haz-git <zhouharry1@gmail.com>
2020-10-01 12:02:55 -0700

    Added Template Rendering to Landing Page

--- index.js
+++ index.js
@@ -7,6 +7,23 @@ const url = require('url');
 
 //__dirname is the directory where this (index.js) current file is located.
 //We are synchronously reading our html templates here.
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    return output;
+}
+
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
@@ -26,7 +43,12 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type': 'text/html'
         })
-        res.end(tempOverview);
+        //Single line arrow functions should not be used with curly braces-- this is why the array was returned undefined for me.
+        const cardsHTML = dataObj.map(el => replaceTemplate(tempCard, el));
+        console.log(cardsHTML);
+
+        const output = tempOverview.replace('{%PRODUCTCARDS%}', cardsHTML);
+        res.end(output);
 
     //Product Page:
 
